small bugs fixed for new versions - torch.amp instead of torch.cuda.amp - in train_stereo sequence_loss modification when valid is not True the backpropagation is not computed - wandb.init entity changed with my personal one

core/model.py
@@ -7,9 +7,10 @@ from core.extractor import MultiBasicEncoder, ResidualBlock
 from core.corr import CorrBlock1D
 from core.utils.utils import coords_grid
 from core.refinement import refineNet
+DEBUG = False
 
 try:
-    autocast = torch.cuda.amp.autocast
+    autocast = torch.amp.autocast
 except:
     # dummy autocast for PyTorch < 1.6
     class autocast:
@@ -73,12 +74,18 @@ class PCVNet(nn.Module):
         return up_x.reshape(n, d, factor * h, factor * w)
 
     def forward(self, image1, image2, iters=12, test_mode=False, init_param=None, cascade=False):
+        if DEBUG:
+            print('------------------------------------------------------------------------------------------')
+            print('Forward function of the model ...')
         self.test_mode = test_mode
         image1 = (2 * (image1 / 255.0) - 1.0).contiguous()
         image2 = (2 * (image2 / 255.0) - 1.0).contiguous()
 
+        if DEBUG:
+            print('Image shapes', image1.shape, image2.shape)
+
         # feature extraction
-        with autocast(enabled=self.args.mixed_precision):
+        with autocast('cuda', enabled=self.args.mixed_precision):
             *cnet_list, x, low_f = self.cnet(torch.cat((image1, image2), dim=0), dual_inp=True,
                                              num_layers=self.args.n_gru_layers)
             fmap1, fmap2 = self.conv2(x).split(dim=0, split_size=x.shape[0] // 2)
@@ -86,6 +93,10 @@ class PCVNet(nn.Module):
             inp_list = [torch.relu(f[1]) for f in cnet_list]
             inp_list = [list(conv(i).split(split_size=conv.out_channels // 3, dim=1)) for i, conv in
                         zip(inp_list, self.context_zqr_convs)]
+        if DEBUG:
+            print(f'fmap1 {fmap1.shape}, fmap2 {fmap2.shape}')
+            # print(f'cnet_list {cnet_list[0].shape}, x {x.shape}, low_f {low_f.shape}')
+            print(f'net_list {net_list[0].shape}, inp_list {inp_list[0][0].shape}')
 
         # correlation
         N, C, H, W = net_list[0].shape
@@ -105,6 +116,8 @@ class PCVNet(nn.Module):
             coords1 = coords0 - mu
         else:
             coords0, coords1 = self.initialize_disp(fmap1)
+            if DEBUG:
+                print(f'coords0 {coords0.shape}, coords1 {coords1.shape} after first initialization')
             N, C, H, W = net_list[0].shape
             sigma = torch.ones(N, self.args.gauss_num, H, W).to(
                 coords0.device) * self.args.init_sigma / (
@@ -118,11 +131,15 @@ class PCVNet(nn.Module):
 
         # iterative update
         for itr in range(iters):
+            if DEBUG:
+                print(f'iteration {itr}')
             coords1 = coords1.detach()
             corr = corr_fn(coords1, sigma, self.test_mode)
             mu = (coords0 - coords1).detach()
+            if DEBUG:
+                print(f'mu {mu.shape}')
             motion_features_list = None
-            with autocast(enabled=self.args.mixed_precision):
+            with autocast('cuda', enabled=self.args.mixed_precision):
                 if self.args.n_gru_layers >= 3 and self.args.slow_fast_gru:
                     net_list, motion_features_list = self.FDM(net_list, inp_list, corr, mu.detach(), w=w.detach(),
                                                               sigma=sigma.detach(),
@@ -145,6 +162,8 @@ class PCVNet(nn.Module):
                                                            iter04=True,
                                                            test_mode=(test_mode and itr < iters - 1),
                                                            motion_features_list=motion_features_list)
+                if DEBUG:
+                    print(f'After GRU layers net_list[0]: {net_list[0].shape} up_mask: {up_mask.shape}, mu: {mu.shape}, sigma: {sigma.shape}, w: {w.shape}')
 
             coords1 = coords0 - mu
             if test_mode and itr < iters - 1:
@@ -152,10 +171,12 @@ class PCVNet(nn.Module):
 
             # disparity regression
             disp = torch.sum(w * mu, dim=1, keepdim=True)
+            if DEBUG:
+                print('disp= sum(w*mu)', disp.shape)
 
             # refinement
             if itr == self.args.valid_iters - 1:
-                with autocast(enabled=self.args.mixed_precision):
+                with autocast('cuda', enabled=self.args.mixed_precision):
                     refined_disp = self.refineNet(w.detach(), sigma.detach(),
                                                   mu.detach(),
                                                   disp.detach(), low_f)
@@ -177,6 +198,8 @@ class PCVNet(nn.Module):
             disp_seqs.append(disp_up)
             sigma_seqs.append(sigma_up)
             w_seqs.append(w_up)
+            if DEBUG:
+                print(f'disp_up {disp_up.shape}, mu_up {mu_up.shape}, sigma_up {sigma_up.shape}, w_up {w_up.shape}')
 
         if cascade:
             init_params={'disp': disp_up,

evaluate_stereo.py
@@ -156,9 +156,9 @@ def validate_kitti(model, iters=32, mixed_prec=False, device=[0], val_set=None, 
 def validate_things(model, iters=32, mixed_prec=False, device=[0], version='frames_finalpass'):
     """ Perform validation using the FlyingThings3D (TEST) split """
     model.eval()
-    val_dataset = datasets.SceneFlowDatasets(dstype=version, things_test=True)
+    val_dataset = datasets.SceneFlowDatasets(root ='/media/goldberg/T91/Datasets/sceneflow', dstype=version, things_test=True)
 
-    out_list, epe_list = [], []
+    out_list, epe_list, elapsed_list = [], [], []
     for val_id in tqdm(range(len(val_dataset))):
         _, image1, image2, disp_gt, valid_gt = val_dataset[val_id]
         image1 = image1[None].cuda(device[0])  # 相当于把第一个N维度扩展了
@@ -167,8 +167,15 @@ def validate_things(model, iters=32, mixed_prec=False, device=[0], version='fram
         padder = InputPadder(image1.shape, divis_by=32)
         image1, image2 = padder.pad(image1, image2)
 
-        with autocast(enabled=mixed_prec):
+        torch.cuda.synchronize()
+        start = time.time()
+        with autocast('cuda', enabled=mixed_prec):
             disp_pr = model(image1, image2, iters=iters, test_mode=True)
+        torch.cuda.synchronize()
+        end = time.time()
+
+        elapsed_list.append(end - start)
+
         disp_pr = padder.unpad(disp_pr).cpu().squeeze(0)  # 1,1,h,w
         assert disp_pr.shape == disp_gt.shape, (disp_pr.shape, disp_gt.shape)
         epe = (disp_pr - disp_gt).abs()
@@ -184,12 +191,15 @@ def validate_things(model, iters=32, mixed_prec=False, device=[0], version='fram
         # out_list.append(out[val].cpu().numpy())
 
     epe_list = np.array(epe_list)
+    elapsed_list = np.array(elapsed_list)
     # out_list = np.concatenate(out_list)
 
     epe = np.mean(epe_list)
+    mean_runtime = np.mean(elapsed_list[1:])
     # d1 = 100 * np.mean(out_list)
 
-    print("Validation FlyingThings: %f" % (epe))
+    print(
+        f"Validation FlyingThings: EPE {format(epe, '.3f')}, {format(1 / mean_runtime, '.2f')}-Time ({format(mean_runtime, '.3f')}s)")
     return {'things-epe': epe}
 
 

train_stereo.py
@@ -13,9 +13,10 @@ import torch.nn.functional as F
 from evaluate_stereo import validate
 import core.stereo_datasets as datasets
 from core.utils.data_parallel import BalancedDataParallel
+DEBUG = False
 
 try:
-    from torch.cuda.amp import GradScaler
+    from torch.amp import GradScaler
 except:
     # dummy GradScaler for PyTorch < 1.6
     class GradScaler:
@@ -74,15 +75,18 @@ def sequence_loss(disp_refine, final_disp_preds, mu_preds, w_preds, sigma_preds,
             w_preds[i] = w_preds[i].view(N // gauss_num, gauss_num, 1, H, W)
             sigma_preds[i] = sigma_preds[i].view(N // gauss_num, gauss_num, 1, H, W)
             w = w_preds[i]
-            print("mu%d:" % i, torch.mean(mu_preds[i], dim=[0, 2, 3, 4]).detach())
-            print("w%d:" % i, torch.mean(w, dim=[0, 2, 3, 4]).detach())
-            print("sigma%d:" % i, torch.mean(sigma_preds[i], dim=[0, 2, 3, 4]).detach())
+            # print("mu%d:" % i, torch.mean(mu_preds[i], dim=[0, 2, 3, 4]).detach())
+            # print("w%d:" % i, torch.mean(w, dim=[0, 2, 3, 4]).detach())
+            # print("sigma%d:" % i, torch.mean(sigma_preds[i], dim=[0, 2, 3, 4]).detach())
 
             i_loss1 = (final_disp_preds[i] - disp_gt).abs()
             i_loss2 = torch.mean((mu_preds[i] - disp_gt[:, None]).abs(), dim=1)
             disp_loss += i_weights[i] * (
                     i_loss1.view(-1)[valid.view(-1)].mean() + i_loss2.view(-1)[valid.view(-1)].mean())
+            # print('i_weights[i] * (i_loss1.view(-1)[valid.view(-1)].mean() + i_loss2.view(-1)[valid.view(-1)].mean())',i_weights[i] * (
+                    # i_loss1.view(-1)[valid.view(-1)].mean() + i_loss2.view(-1)[valid.view(-1)].mean()))
         disp_loss += 1.4 * F.smooth_l1_loss(disp_refine[valid], disp_gt[valid], size_average=True)
+        # print('F.smooth_l1_loss(disp_refine[valid], disp_gt[valid], size_average=True)', F.smooth_l1_loss(disp_refine[valid], disp_gt[valid], size_average=True))
 
         epe_final = torch.abs(disp_refine - disp_gt)
         epe_final = epe_final.view(-1)[valid.view(-1)]
@@ -106,6 +110,10 @@ def sequence_loss(disp_refine, final_disp_preds, mu_preds, w_preds, sigma_preds,
             'bad2_final': (epe_final > 2).float().mean().item(),
             'bad5_final': (epe_final > 5).float().mean().item(),
         }
+
+    # print('Inside sequence loss')
+    # print('disp_loss:', disp_loss)
+    # print('metrics:', metrics)
 
     return disp_loss, metrics
 
@@ -190,11 +198,11 @@ def train(args):
     val_idxs = set(np.random.permutation(200)[:40])
     np.random.set_state(state)
     args.valid_set = val_idxs
-
+    print('Train Loader ...')
     train_loader = datasets.fetch_dataloader(args, occ_mask=False)
     optimizer, scheduler = fetch_optimizer(args, model)
     logger = Logger(model, scheduler, args.log_freq)
-    scaler = GradScaler(enabled=args.mixed_precision)
+    scaler = GradScaler('cuda', enabled=args.mixed_precision)
 
     # load checkpoint
     if args.restore_ckpt is not None:
@@ -229,10 +237,13 @@ def train(args):
     gauss_num_train = args.gauss_num
     best_metric = float('inf')
     # training
+    print('Start training. ')
     while should_keep_training:
         for i_batch, (files_path, *data_blob) in enumerate(tqdm(train_loader)):
             optimizer.zero_grad()
             image1, image2, disp, valid = [x.cuda(args.device[0]) for x in data_blob]
+            # print('image1', image1.shape)
+            # print('image2', image2.shape)
 
             if args.cascade:
                 image1_dw2 = F.interpolate(image1, scale_factor=(0.5, 0.5), mode='bilinear', align_corners=True)
@@ -243,6 +254,8 @@ def train(args):
 
             assert model.training
             output_list = model(image1, image2, iters=args.train_iters, init_param=init_param)
+            # print('output list', len(output_list))
+            # print(output_list[0], output_list[1], output_list[2], output_list[3], output_list[4])   
             assert model.training
 
             # calculate loss
@@ -256,20 +269,27 @@ def train(args):
                 loss += 0.5 * loss_dw2
             else:
                 loss, metrics = sequence_loss(*output_list, disp, valid, gauss_num_train, args.max_disp)
+            
 
             # log
             logger.push(metrics)
-            logger.writer.log({"live_loss": loss.item(), 'learning_rate': optimizer.param_groups[0]['lr']})
+            try:
+                logger.writer.log({"live_loss": loss.item(), 'learning_rate': optimizer.param_groups[0]['lr']})
+            except:
+                logger.writer.log({"live_loss": loss, 'learning_rate': optimizer.param_groups[0]['lr']})
 
-            # gradient scaling
-            scaler.scale(loss).backward()
-            scaler.unscale_(optimizer)
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+            # # gradient scaling
+            # print('Loss before scale(loss)', loss)
+            # print('try loss item', loss.item())
+            if loss != 0:
+                scaler.scale(loss).backward()
+                scaler.unscale_(optimizer)
+                torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
 
-            # backward
-            scaler.step(optimizer)
-            scheduler.step()
-            scaler.update()
+                # backward
+                scaler.step(optimizer)
+                scheduler.step()
+                scaler.update()
 
             # validation
             if total_steps % validation_frequency == validation_frequency - 1:
@@ -359,7 +379,7 @@ if __name__ == '__main__':
     wandb.init(
         job_type="train",
         project=args.name,
-        entity="zengjiaxi"
+        entity="elenagovi0-universit-di-modena-e-reggio-emilia"
     )
     # add the args to wandb
     wandb.config.update(args)